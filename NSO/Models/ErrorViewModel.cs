7a94eb29c0b138b9a43d212be2768d83a0f93c62
Sss <surkun1992@gmail.com>
2026-02-28 18:44:55 +0700

    Добавьте файлы проекта.

---
+++ NSO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NSO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
